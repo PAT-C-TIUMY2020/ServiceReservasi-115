170be551836d6e5ff749057c0dc268434a23c69e
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-11-28 15:07:40 +0700

    wcf crud

--- IService1.cs
+++ IService1.cs
@@ -15,7 +15,7 @@ namespace ServiceReservasi
         string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi);//method proses input data
 
         [OperationContract]
-        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        string editPemesanan(string IDPemesanan, string NamaCustomer, string No_telpon);
 
         [OperationContract]
         string deletePemesanan(string IDPemesanan);
@@ -82,7 +82,7 @@ namespace ServiceReservasi
         public int JumlahPemesanan { get; set; }
 
         [DataMember]
-        public string IDLokasi { get; set; }
+        public string Lokasi { get; set; }
     }
 
 
